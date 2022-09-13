45db53e76bf5ee166658743b06a222b87a73aa06
Thiago Macedo <thialmacedo@gmail.com>
2022-09-13 18:39:52 -0300

    docstring for node

--- node.py
+++ node.py
@@ -5,6 +5,7 @@ class Node():
 
 
     def __init__(self, jar1: Jar, jar2: Jar, jar3: Jar) -> None:
+        """Initializes the node object with three jars"""
         self.jar1 = jar1
         self.jar2 = jar2
         self.jar3 = jar3
@@ -13,11 +14,21 @@ class Node():
 
 
     def add_child(self, n) -> None:
+        """Adds a child to a node
+
+        Args:
+            n (Node): child that will be added to the node
+        """        
         self.children.append(n)
         n.parent = self
 
 
     def copy(self):    
+        """Creates a copy of node
+
+        Returns:
+            Node: Identical copy of the node
+        """        
         jars = [self.jar1, self.jar2, self.jar3]
         new_jars = []
         for jar in jars:
@@ -27,7 +38,12 @@ class Node():
         return Node(new_jars[0], new_jars[1], new_jars[2])
 
 
-    def create_copies(self) -> list:     
+    def create_copies(self) -> list:    
+        """Creates 6 identical copies of a node
+
+        Returns:
+            list: list of identical nodes
+        """         
         nodes = []
 
         for _ in range(6):
@@ -38,6 +54,11 @@ class Node():
 
 
     def combinations(self) -> list:
+        """All the combinations of jars that a node can have
+
+        Returns:
+            list: list of combinations
+        """        
         combinations = [
             (self.jar1, self.jar2),
             (self.jar1, self.jar3),
@@ -51,17 +72,46 @@ class Node():
 
 
     def state(self):
+        """Current amount of water in each jar for a node
+
+        Returns:
+            tuple: tuple with the 3 amount of water for the three nodes
+        """        
         return tuple([self.jar1.current_amount, self.jar2.current_amount, self.jar3.current_amount])
 
     
     def water_dump(self, jar_gives: Jar, jar_receives: Jar) -> None:
+        """Performs water dump between two jars for a given node
+
+        Args:
+            jar_gives (Jar): jar that gives water
+            jar_receives (Jar): jar that receives water
+        """        
         jar_gives, jar_receives = Jar.water_dump(jar_gives, jar_receives)
 
     def check_water_dump_possibility(self, jar_gives, jar_receives):
+        """Check if it's possible to perform a water dump between two jars for a given node
+
+        Args:
+            jar_gives (Jar): jar that gives water
+            jar_receives (Jar): jar that receives water
+
+        Returns:
+            bool: true if it's possible to perform water dump
+        """        
         return Jar.check_water_dump_possibility(jar_gives, jar_receives)
 
 
 def check_solution(node: Node):
+    """Function to check if the node has reached the solution, that is, for all nodes, 
+       the current amount of water is equal to the desired amount of water
+
+    Args:
+        node (Node): node that will be tested
+
+    Returns:
+        bool: if the node is the solution
+    """    
     jar1_done = node.jar1.current_amount == node.jar1.desired_amount
     jar2_done = node.jar2.current_amount == node.jar2.desired_amount
     jar3_done = node.jar3.current_amount == node.jar3.desired_amount
