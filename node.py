9bcf07acd9100ef2ef72e55db93c722d95c8d52c
Thiago Macedo <thialmacedo@gmail.com>
2022-09-13 12:29:25 -0300

    check_solution method

--- node.py
+++ node.py
@@ -4,39 +4,20 @@ from jar import Jar
 class Node():
 
 
-    def __init__(self, jar1: Jar, jar2: Jar, jar3: Jar, children: list = [], parent = None) -> None:
-        """_summary_
-
-        Args:
-            jar1 (Jar): _description_
-            jar2 (Jar): _description_
-            jar3 (Jar): _description_
-            children (list, optional): _description_. Defaults to [].
-            parent (_type_, optional): _description_. Defaults to None.
-        """        
+    def __init__(self, jar1: Jar, jar2: Jar, jar3: Jar) -> None:
         self.jar1 = jar1
         self.jar2 = jar2
         self.jar3 = jar3
-        self.children = children
-        self.parent = parent
+        self.children = []
+        self.parent = None
 
 
-    def add_child(self, n):
-        """_summary_
-
-        Args:
-            n (_type_): _description_
-        """        
+    def add_child(self, n) -> None:
         self.children.append(n)
         n.parent = self
 
 
-    def copy(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """                
+    def copy(self):    
         jars = [self.jar1, self.jar2, self.jar3]
         new_jars = []
         for jar in jars:
@@ -46,12 +27,17 @@ class Node():
         return Node(new_jars[0], new_jars[1], new_jars[2])
 
 
-    def combinations(self) -> list:
-        """_summary_
+    def create_copies(self) -> list:     
+        nodes = []
 
-        Returns:
-            list: _description_
-        """        
+        for _ in range(6):
+            n = self.copy()
+            nodes.append(n)
+
+        return nodes
+
+
+    def combinations(self) -> list:
         combinations = [
             (self.jar1, self.jar2),
             (self.jar1, self.jar3),
@@ -64,63 +50,20 @@ class Node():
         return combinations
 
 
-    def check_water_dump_possible(self, jar_gives: Jar, jar_receives: Jar) -> bool:
-        """_summary_
+    def state(self):
+        return tuple([self.jar1.current_amount, self.jar2.current_amount, self.jar3.current_amount])
 
-        Args:
-            jar_gives (Jar): _description_
-            jar_receives (Jar): _description_
-
-        Returns:
-            bool: _description_
-        """        
-        return Jar.check_water_dump_possibility(jar_gives, jar_receives)
-
-
+    
     def water_dump(self, jar_gives: Jar, jar_receives: Jar) -> None:
         jar_gives, jar_receives = Jar.water_dump(jar_gives, jar_receives)
-
 
     def check_water_dump_possibility(self, jar_gives, jar_receives):
         return Jar.check_water_dump_possibility(jar_gives, jar_receives)
 
 
-    def state(self):
-        """_summary_
+def check_solution(node: Node):
+    jar1_done = node.jar1.current_amount == node.jar1.desired_amount
+    jar2_done = node.jar2.current_amount == node.jar2.desired_amount
+    jar3_done = node.jar3.current_amount == node.jar3.desired_amount
 
-        Returns:
-            _type_: _description_
-        """        
-        return tuple([self.jar1.current_amount, self.jar2.current_amount, self.jar3.current_amount])
-    
-
-    def check_solution(self) -> bool:
-        """_summary_
-
-        Returns:
-            bool: _description_
-        """        
-        jar1_done = self.jar1.current_amount == self.jar1.desired_amount
-        jar2_done = self.jar2.current_amount == self.jar2.desired_amount
-        jar3_done = self.jar3.current_amount == self.jar3.desired_amount
-
-        return jar1_done and jar2_done and jar3_done
-    
-
-    def create_copies(self) -> list:
-        """_summary_
-
-        Returns:
-            list: _description_
-        """        
-        nodes = []
-
-        for _ in range(6):
-            n = self.copy()
-            nodes.append(n)
-
-        return nodes
-
-
-    def print_node(self):
-        print({self.jar1.current_amount, self.jar2.current_amount, self.jar3.current_amount})
+    return jar1_done and jar2_done and jar3_done
