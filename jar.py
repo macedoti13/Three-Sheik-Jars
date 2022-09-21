e2cec605857a1f3ef530a210244ad0f457140397
Thiago Macedo <thialmacedo@gmail.com>
2022-09-21 10:07:00 -0300

    docstrings in jar module

--- jar.py
+++ jar.py
@@ -2,18 +2,18 @@ class Jar():
 
 
     def __init__(self, capacity: int, current_amount: int, desired_amount: int) -> None:  
-        """Initializes the jar object
+        """Initializes a Jar object
 
         Args:
-            capacity (int): maximum amount of water it can take
-            current_amount (int): amount of water it starts with
-            desired_amount (int): amount of water it needs to have
+            capacity (int): Amount of water the jar can take
+            current_amount (int): Amount of water the jar currently has
+            desired_amount (int): Amount of water the jar needs to end up with
 
         Raises:
-            Exception: can't have negative or above 40 liters of water
-            Exception: can't initiate with more water than it can take
-            Exception: can't require more water that it can take
-        """                    
+            Exception: Capacity can't be negative or above 40
+            Exception: Jar can't have negative water or more water than it can take
+            Exception: Jar can't need negative water or more water than it can't take
+        """                   
         if capacity > 40 or capacity < 0:
             raise Exception
         elif current_amount > capacity or current_amount < 0:
@@ -53,14 +53,15 @@ class Jar():
             self.__current_amount = new_amount
 
 
-    def remove_water(self, amount: int) -> None:
-        """Removes water from a jar
+    def remove_water(self, amount: int) -> None:  
+        """Removes an amount water from a jar by subtracting this amount from the 
+           current_amount.
 
         Args:
-            amount (int): amount of water that will be removed
+            amount (int): Amount of water that will be removed from the jar
 
         Raises:
-            Exception: can remove negative water or more water then it has
+            Exception: Can't remove negative water or remove more than the jar has
         """        
         if amount < 0 or amount > self.__current_amount:
             raise Exception
@@ -68,14 +69,15 @@ class Jar():
         self.__current_amount -= amount
 
 
-    def add_water(self, amount: int) -> None:
-        """Adds water to a jar
+    def add_water(self, amount: int) -> None:      
+        """Adds an certain amount of water in a jar by adding this amount 
+           to the current_amount.
 
         Args:
-            amount (int): amount of water that will be added
+            amount (int): Amount of wataer that will be added to the jar
 
         Raises:
-            Exception: can't add negative water or surpass the capacity of the jar
+            Exception: Can't add negative water or surpass the jar's capacity
         """        
         if amount < 0 or amount + self.__current_amount > self.__capacity:
             raise Exception
@@ -83,34 +85,36 @@ class Jar():
         self.__current_amount += amount
 
     
-    def full(self) -> bool:    
-        """Checks if a jar is full a water
+    def full(self) -> bool: 
+        """Checks if a jar is full of water by comparing the current_amount 
+           with the capacity
 
         Returns:
-            bool: True is current amount equals to capacity
-        """        
+            bool: True if the jar is full else False
+        """                 
         return self.current_amount == self.capacity
 
 
-    def empty(self) -> bool:   
-        """Checks if a jar is empty
+    def empty(self) -> bool:         
+        """Checks if a jar is empty by looking at the current amount
 
         Returns:
-            bool: True if current amount is 0
-        """        
+            bool: True of jar has no water False otherwise
+        """
         return self.current_amount == 0
         
 
     @staticmethod
-    def water_dump(jar_gives, jar_receives):
-        """Puts water from a jar to another 
+    def water_dump(jar_gives, jar_receives):   
+        """Dumps water from a jar to another jar. Dumps all the water from the jar_gives into
+           the jar_receives or fills up the jar_receives. 
 
         Args:
-            jar_gives (Jar): jar that will give the water
-            jar_receives (Jar): jar that will receive the water
+            jar_gives (Jar): Jar that will lose water
+            jar_receives (Jar): Jar the will receive water
 
         Returns:
-            Jar: both jars with new amounts of water
+            Jar: returns the two jar objects with the new amount of water after the dump
         """        
         jar_receives_remaining_capacity = jar_receives.capacity - jar_receives.current_amount
 
@@ -125,16 +129,18 @@ class Jar():
 
 
     @staticmethod
-    def check_water_dump_possibility(jar_gives, jar_receives):
-        """Checks if it's possible to perform water dump from a jar to another
+    def check_water_dump_possibility(jar_gives, jar_receives): 
+        """Checks if it's possible to do a water dump between two jars. It's not
+           possible when the jar that gives is empty or the jar that receives is 
+           full.
 
         Args:
-            jar_gives (Jar): jar that will give the water
-            jar_receives (Jar): jar that will receive the water
+            jar_gives (Jar): Jar that gives water (can't be empty)
+            jar_receives (Jar): Jar that receives water (can't be full)
 
         Returns:
-            bool: True if it's possible to perform water dump between jars
-        """        
+            bool: True if the water dump is possible False otherwise
+        """             
         if jar_gives.empty() or jar_receives.full():
             return False
         
